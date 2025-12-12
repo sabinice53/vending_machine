e7fb423a4dfdd338fe1a1e5f7e9538129e8ec081
Sabina Osmonova <sabinice54@gmail.com>
2025-12-12 16:21:44 +0600

    Updated CoinAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,50 @@
 package model;
 
-public class CoinAcceptor {
+import interfaces.PaymentAcceptor;
+
+import java.util.Scanner;
+
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
+
+    public Scanner sc = new Scanner(System.in);
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
-    public int getAmount() {
+    @Override
+    public int getBalance() {
         return amount;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    @Override
+    public void topUp() {
+        print("Введите сумму монет для пополнения: ");
+        String sumOfBalance = sc.nextLine().strip();
+
+        try {
+            int add = Integer.parseInt(sumOfBalance);
+            if (add <= 0) {
+                print("Сумма должна быть больше 0.");
+                return;
+            }
+            amount += add;
+        } catch (NumberFormatException e) {
+            print("Нужно ввести число.");
+        }
+    }
+
+    @Override
+    public boolean charge(int amountToCharge) {
+        if (amount >= amountToCharge) {
+            amount -= amountToCharge;
+            return true;
+        }
+        return false;
+    }
+
+    public void print(String str) {
+        System.out.println(str);
     }
 }
